7417558236f66799d656bef595634108f3348242
Yeu <yeumensilva9@gmail.com>
2026-05-07 21:12:03 -0300

    add map view using OpenStreetMap

---
+++ app/reportMap/page.tsx
@@ -0,0 +1,8 @@
+import ReportMapView from "@/components/reportMap/ReportMapView";
+import React from "react";
+
+const ReportMap = () => {
+  return <ReportMapView />;
+};
+
+export default ReportMap;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "User-Agent",
+            value: "MendozaReporta/1.0 (contacto@tuapp.com)",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
